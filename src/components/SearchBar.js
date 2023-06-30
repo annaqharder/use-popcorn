3ffa7ad16c788f210c449c59ad5dca9925dc47d5
Anna Harder <annaqharder@gmail.com>
2023-06-30 15:13:31 -0600

    added search focus

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function SearchBar({ query, setQuery }) {
+  // 1) create ref
+  const inputElement = useRef(null);
+
+  // 3) have to use useEffect for the ref
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputElement.current) return;
+        if (e.code === "Enter") {
+          inputElement.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
@@ -8,6 +27,8 @@ function SearchBar({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      // 2) select the element
+      ref={inputElement}
     />
   );
 }
