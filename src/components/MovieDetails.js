0553e8806f5a30a7506649c5aa738d504d103cc8
Anna Harder <annaqharder@gmail.com>
2023-06-29 22:46:40 -0600

    added movie details + star ratings

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -2,9 +2,22 @@ import { useEffect, useState } from "react";
 import StarRating from "./StarRating";
 import Loader from "./Loader";
 
-function MovieDetails({ selectedId, onCloseMovie, KEY }) {
+function MovieDetails({
+  selectedId,
+  onCloseMovie,
+  KEY,
+  onAddWatched,
+  watched,
+}) {
   const [movie, setMovie] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  const [userRating, setUserRating] = useState("");
+
+  const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
+
+  const watchedUserRating = watched.find(
+    (movie) => movie.imdbID === selectedId
+  )?.userRating;
 
   const {
     Title: title,
@@ -19,6 +32,19 @@ function MovieDetails({ selectedId, onCloseMovie, KEY }) {
     Genre: genre,
   } = movie;
 
+  function handleAdd() {
+    const newWatchedMovie = {
+      imdbID: selectedId,
+      title,
+      year,
+      poster,
+      imdbRating: Number(imdbRating),
+      runtime: Number(runtime.split(" ").at(0)),
+      userRating,
+    };
+    onAddWatched(newWatchedMovie);
+    onCloseMovie();
+  }
   useEffect(
     function () {
       async function getMovieDetails() {
@@ -45,9 +71,8 @@ function MovieDetails({ selectedId, onCloseMovie, KEY }) {
             <button className="btn-back" onClick={onCloseMovie}>
               &larr;
             </button>
-            <img src={poster} alt={`Poster of ${movie} movie`} />
+            <img src={poster} alt={`Poster of ${movie.title} movie`} />
             <div className="details-overview">
-              {" "}
               <h2>{title}</h2>
               <p>
                 {released} &bull; {runtime}
@@ -61,7 +86,26 @@ function MovieDetails({ selectedId, onCloseMovie, KEY }) {
           </header>
           <section>
             <div className="rating">
-              <StarRating maxRating={10} size={24} />
+              {!isWatched ? (
+                <>
+                  {" "}
+                  <StarRating
+                    maxRating={10}
+                    size={24}
+                    onSetRating={setUserRating}
+                  />
+                  {userRating > 0 && (
+                    <button className="btn-add" onClick={handleAdd}>
+                      + Add to list
+                    </button>
+                  )}{" "}
+                </>
+              ) : (
+                <p>
+                  You have already rated this movie {watchedUserRating}
+                  <span>⭐️</span>
+                </p>
+              )}
             </div>
 
             <p>
