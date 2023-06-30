9e622b7de388070ed5ddc597dfaef5770a72217e
Anna Harder <annaqharder@gmail.com>
2023-06-30 08:45:06 -0600

    added tab name change on new movie

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -61,6 +61,14 @@ function MovieDetails({
     [selectedId]
   );
 
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
