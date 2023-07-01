5beced1c313c175f886e6d484f722a0df804342b
Anna Harder <annaqharder@gmail.com>
2023-07-01 16:53:15 -0600

    used useKey hook in SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,32 @@
 import React, { useEffect, useRef } from "react";
+import { useKey } from "../hooks/useKey";
 
 function SearchBar({ query, setQuery }) {
   // 1) create ref
   const inputElement = useRef(null);
 
-  // 3) have to use useEffect for the ref
-  useEffect(
-    function () {
-      function callback(e) {
-        if (document.activeElement === inputElement.current) return;
-        if (e.code === "Enter") {
-          inputElement.current.focus();
-          setQuery("");
-        }
-      }
-      document.addEventListener("keydown", callback);
-      return () => document.addEventListener("keydown", callback);
-    },
-    [setQuery]
-  );
+  useKey("Enter", function () {
+    if (document.activeElement === inputElement.current) return;
+
+    inputElement.current.focus();
+    setQuery("");
+  });
+
+  // 3) have to use useEffect for the ref --> transformed into useKey hook ^
+  // useEffect(
+  //   function () {
+  //     function callback(e) {
+  //       if (document.activeElement === inputElement.current) return;
+  //       if (e.code === "Enter") {
+  //         inputElement.current.focus();
+  //         setQuery("");
+  //       }
+  //     }
+  //     document.addEventListener("keydown", callback);
+  //     return () => document.addEventListener("keydown", callback);
+  //   },
+  //   [setQuery]
+  // );
 
   return (
     <input
